8e23099a0070f03012fddc3f542a5a56ae6f3b48
Maycon Douglas <mkydouglas@gmail.com>
2025-12-14 16:57:36 -0300

    First commit

---
+++ src/main/java/br/com/mkydouglas/mini_picpay/MiniPicpayApplication.java
@@ -0,0 +1,13 @@
+package br.com.mkydouglas.mini_picpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniPicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniPicpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mkydouglas/mini_picpay/MiniPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mkydouglas.mini_picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniPicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
