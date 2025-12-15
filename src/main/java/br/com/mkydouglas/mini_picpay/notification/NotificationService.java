ba9a7ee8fe1b37254bab25f7056ffe9b1e33bbc9
Maycon Douglas <mkydouglas@gmail.com>
2025-12-15 07:42:05 -0300

    Implement NotificationService

--- src/main/java/br/com/mkydouglas/mini_picpay/notification/NotificationService.java
+++ src/main/java/br/com/mkydouglas/mini_picpay/notification/NotificationService.java
@@ -6,8 +6,13 @@ import br.com.mkydouglas.mini_picpay.transaction.Transaction;
 
 @Service
 public class NotificationService {
-    
+    private final NotificationProducer notificationProducer;
+
+    public NotificationService(NotificationProducer notificationProducer) {
+        this.notificationProducer = notificationProducer;
+    }
+
     public void notify(Transaction transaction) {
-        
+        notificationProducer.sendNotification(transaction);
     }
 }
